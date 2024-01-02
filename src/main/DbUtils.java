e049d64402e48a123b89ba3429d620cf862359dd
DianeArinimbabazi <154262284+DianeArinimbabazi@users.noreply.github.com>
2024-01-02 05:33:12 -0800

    pharmarcy project
    MY REG NUMBER:  Diane_ ARINIMBABAZI_222014124_group2

---
+++ src/main/DbUtils.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.sql.ResultSet;
+import javax.swing.table.TableModel;
+
+/**
+ *
+ * @author Administrator
+ */
+class DbUtils {
+
+    static TableModel resultSetToTableModel(ResultSet res) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
